be60a2596816b71ee7fbd367d65b37c4c6b87524
Форов Александр <5h9t5@list.ru>
2024-07-18 20:00:41 +0300

    pipelines(feat): add logic create file with count and total pep status

--- pep_parse/pipelines.py
+++ pep_parse/pipelines.py
@@ -5,9 +5,37 @@
 
 
 # useful for handling different item types with a single interface
-from itemadapter import ItemAdapter
+import csv
+import datetime as dt
+from collections import defaultdict
+
+from pep_parse.settings import BASE_DIR, RESULTS, DATETIME_FORMAT
 
 
 class PepParsePipeline:
+    def __init__(self):
+        self.results_dir = BASE_DIR / RESULTS
+        self.results_dir.mkdir(exist_ok=True)
+
+    def open_spider(self, spider):
+        self.statuses = defaultdict(int)
+
+    def close_spider(self, spider):
+        now = dt.datetime.now()
+        now_formatted = now.strftime(DATETIME_FORMAT)
+        file_name = f'status_summary_{now_formatted}.csv'
+        file_path = self.results_dir / file_name
+        with open(file_path, mode='w', encoding='utf-8') as csvfile:
+            csv.writer(
+                csvfile,
+                dialect=csv.unix_dialect,
+                quoting=csv.QUOTE_NONE,
+            ).writerows([
+                ('Status', 'Quantity'),
+                *self.statuses.items(),
+                ('Total', sum(self.statuses.values())),
+            ])
+
     def process_item(self, item, spider):
+        self.statuses[item.get('status')] += 1
         return item
